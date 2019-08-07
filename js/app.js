10b993a6e94971d3a1938dd49425bdf434035b24
Chaz Kondo <Chaz@Macbook-Pro-2.local>
2019-08-06 18:40:51 -1000

    First repo

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+
+var audio = new Audio('../audio/pizza.mp3');
+audio.play();
+audio.loop = true;
